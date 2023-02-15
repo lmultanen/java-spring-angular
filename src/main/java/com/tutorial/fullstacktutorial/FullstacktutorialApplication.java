54707affa5fccf9d806830aa386d31e3a1f75898
Luke Multanen <lmultanen.uchicago@gmail.com>
2023-02-15 08:40:51 -0600

    initial spring boot files

---
+++ src/main/java/com/tutorial/fullstacktutorial/FullstacktutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.fullstacktutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacktutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacktutorialApplication.class, args);
+	}
+
+}
